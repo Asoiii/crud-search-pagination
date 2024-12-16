d9929a06d43cbe792459137b3b91fbf70826f0b8
Asoiii <muhammmadhamzah159@gmail.com>
2024-12-16 09:48:20 +0700

    first commit

---
+++ app/contacts/edit/[id]/page.tsx
@@ -0,0 +1,12 @@
+import CreateForm from "@/app/components/CreateForm";
+
+const UpdateContactPage = () => {
+  return (
+    <div className="max-w-md mx-auto mt-5">
+      <h1 className="text-2xl text-center mb-2">Update Contact</h1>
+      <CreateForm />
+    </div>
+  );
+};
+
+export default UpdateContactPage;
